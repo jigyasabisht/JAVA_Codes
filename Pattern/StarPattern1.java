d8539c200061598056bd79f2273bdfcf81913f78
Jigyasa Bisht <67429151+jigyasabisht@users.noreply.github.com>
2021-10-20 13:50:35 +0530

    Create StarPattern1.java

---
+++ Pattern/StarPattern1.java
@@ -0,0 +1,20 @@
+// Increasing Traingle Pattern
+// "*" in increasing traingle form
+
+import java.util.*;
+public class StarPattern1
+{
+	public static void main(String[] args) 
+	{
+	    Scanner sc =  new Scanner(System.in);
+	    int n=sc.nextInt();
+	    for(int i=1;i<=n;i++)
+	    {
+	        for(int j=1;j<=i;j++)
+	        {
+	            System.out.print("*");
+	        }
+		System.out.println();
+	    }
+	}
+}
