6db3daabb33c4421266643208616707433e05fd2
Jigyasa Bisht <67429151+jigyasabisht@users.noreply.github.com>
2021-10-21 10:49:49 +0530

    Create StarPattern6.java

---
+++ Pattern/StarPattern6.java
@@ -0,0 +1,34 @@
+// Pyramid Pattern
+
+/*
+
+    *
+   ***
+  *****
+ *******
+*********
+
+*/
+
+//Code
+import java.util.*;
+public class StarPattern6
+{
+	public static void main(String[] args) 
+	{
+	    Scanner sc = new Scanner(System.in);
+	    int n = sc.nextInt();
+	    for(int i=1;i<=n;i++)
+	    {
+	        for(int j=n;j>i;j--)
+	        {
+	            System.out.print(" ");
+	        }
+	        for(int k=1;k<(i*2);k++)
+	        {
+	            System.out.print("*");
+	        }
+	        System.out.println();
+	    }
+	}
+}
