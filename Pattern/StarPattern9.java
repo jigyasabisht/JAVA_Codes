ab75e43f7a5caf392f5ec568ad799c049347f426
Jigyasa Bisht <67429151+jigyasabisht@users.noreply.github.com>
2021-10-25 14:31:27 +0530

    Create StarPattern9.java

---
+++ Pattern/StarPattern9.java
@@ -0,0 +1,36 @@
+// Diagonal Star Pattern
+
+/*
+
+*
+ *
+  *
+   *
+    *
+    
+*/
+
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) 
+	{
+	    Scanner sc = new Scanner(System.in);
+	    int n = sc.nextInt();
+	    for(int i=1;i<=n;i++)
+	    {
+	        for(int j=1;j<=i;j++)
+	        {
+	            if(i>=2 && j<=i-1)
+	            {
+	                System.out.print(" ");
+	            }
+	            else{
+	                System.out.print("*");
+	            }
+	        }
+	        System.out.println();
+	    }
+		
+	}
+}
